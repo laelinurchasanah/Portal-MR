11c9de49ea8b2e2fcb0ecdcfc0221149bc2c6745
laelinurchasanah <laelicha29@gmail.com>
2026-08-12 13:00:48 +0700

    portal-mr-bylael

---
+++ data.js
@@ -0,0 +1,64 @@
+const modulData = [
+    {
+      id: 0x1,
+      title: "Dasar\x20Manajemen\x20Risiko",
+      type: "materi",
+      description:
+        "Permen\x20ATR/BPN\x20No\x201\x20Tahun\x202026\x20tentang\x20Manajemen\x20Risiko",
+      flipUrl:
+        "https://online.fliphtml5.com/jhqkx/Permen-atrbpn-no-1-tahun-2026/",
+    },
+    {
+      id: 0x2,
+      title: "Sosialisasi\x20Permen\x20ATR/BPN\x20No\x201\x20Tahun\x202026",
+      type: "materi",
+      description:
+        "Ringkasan\x20Permen\x20ATR/BPN\x20No\x201\x20Tahun\x202026.",
+      flipUrl: "https://online.fliphtml5.com/example/modul2/",
+    },
+    {
+      id: 0x3,
+      title: "SPIP\x20MR",
+      type: "materi",
+      description:
+        "SPIP\x20pada\x20Tindaklanjut\x20Manajemen\x20Risiko\x20Kementerian",
+      flipUrl: "https://online.fliphtml5.com/jhqkx/SPIP-MR/",
+    },
+    {
+      id: 0x4,
+      title: "Panduan\x20Pengisian\x20Risk\x20Register",
+      type: "materi",
+      description:
+        "Tata\x20cara\x20pengisian\x20Risk\x20Register\x20pada\x20Satuan\x20kerja",
+      flipUrl:
+        "https://online.fliphtml5.com/jhqkx/Panduan-Penyusunan-Risk-Register_2026/",
+    },
+    {
+      id: 0x5,
+      title:
+        "Laporan\x20Manajemen\x20Risiko\x20Triwulan\x20II\x20Tahun\x202026",
+      type: "laporan",
+      description:
+        "Laporan\x20Manajemen\x20Risiko\x20yang\x20disusun\x20berdasarkan\x20risk\x20register\x20pada\x20satuan\x20kerja",
+      flipUrl:
+        "https://online.fliphtml5.com/jhqkx/Laporan-MR-TW-II_Biro-Humas-dan-Protokol/",
+    },
+  ],
+  riskRegisterDriveData = {
+    q1: {
+      embedUrl:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vSKaF-5lVCs5I4HU45GO4F4N0wHTvJoQC_JD1-gJaB4u_nH7feBPU8m6BR-58c-XA/pubhtml",
+      directUrl:
+        "https://docs.google.com/spreadsheets/d/1D82Y6WXCbzGghQAg_pKQjKDY08qk9YOp/edit?usp=sharing&ouid=110118457664365031197&rtpof=true&sd=true",
+    },
+    q2: {
+      embedUrl:
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vT_EKTfNjKhGteg-muawEslc5Qe_afVRt5MnyXkzMYf0ZbKQXs6_k9OmecwWCNInA/pubhtml",
+      directUrl:
+        "https://docs.google.com/spreadsheets/d/1FHs1zHzad-RtWu2qPGdJC71WdSSKBCB5/edit?usp=sharing&ouid=110118457664365031197&rtpof=true&sd=true",
+    },
+    q3: { embedUrl: "", directUrl: "" },
+    q4: { embedUrl: "", directUrl: "" },
+  };
+globalThis["modulData"] = modulData;
+globalThis["riskRegisterDriveData"] = riskRegisterDriveData;
